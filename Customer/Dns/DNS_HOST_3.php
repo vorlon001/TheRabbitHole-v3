67ca9fd62acb799b70c2edee4650479ea08d8737
Denis Kosov <vorlon@MacBook-Pro-Denis.local>
2018-01-20 20:52:20 +0500

    build 43 patch

--- Customer/Dns/DNS_HOST_3.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Customer\Dns;
-
-class DNS_HOST_3 {
-    static function run ($req) { var_dump($req); echo 'DNS HOST 3'.PHP_EOL; return ['id'=>2345234,'txt'=>['a1'=>1234123,'a2'=>'DNS HOST 3']]; }
-}
-?>
